bfe3e27d40456211c27016457d381fce4f6474fa
felipe428 <felipe.silva428@fatec.sp.gov.br>
2021-08-09 12:28:45 -0300

    teste1

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 // Import stylesheets
 import './style.css';
 
+//EX1 console.log("Olá TypeScript!");
+for(let i = 0; i <= 10; i++){
+  console.log(i);
+};
+
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>Teste</h1>`;
